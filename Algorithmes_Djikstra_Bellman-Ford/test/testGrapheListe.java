418d7f92388a7f3e63ba3fc5efe2fd8d286f2009
Valentin ROHR <valentin.rohr9@etu.univ-lorraine.fr>
2026-05-31 21:49:30 +0200

    le rapport et les tests qu'on avait oublie

--- Algorithmes_Djikstra_Bellman-Ford/test/testGrapheListe.java
+++ Algorithmes_Djikstra_Bellman-Ford/test/testGrapheListe.java
@@ -1,3 +1,5 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * on fait des test pour verifier si tout va bien
  */
@@ -5,7 +7,66 @@ public class testGrapheListe {
 
     private GrapheListe graphe;
 
-    public void testNombreNoeuds() {
+    public void init(){
+        graphe = new GrapheListe();
+        graphe.ajouterNoeud("A");
+        graphe.ajouterNoeud("B");
+        graphe.ajouterNoeud("C");
+        graphe.ajouterNoeud("D");
+        graphe.ajouterNoeud("E");
+
+        graphe.ajouterArc("A","B",12.0);
+        graphe.ajouterArc("A","D",87.0);
+        graphe.ajouterArc("B","E",11.0);
+        graphe.ajouterArc("C","A",19.0);
+        graphe.ajouterArc("D","B",23.0);
+        graphe.ajouterArc("D","C",10.0);
+        graphe.ajouterArc("E","D",43.0);
     }
+    // test pour le graphe
+    public void testNombreNoeuds() {
+        assertEquals(5, graphe.getNoeuds().size());
+    }
+
+    public void testArcsDeA(){
+        assertEquals(2, graphe.getNoeuds().size());
+    }
+
+    public void testNoeudsInconnu(){
+        assertThrows(Error.class, ()-> graphe.getArcs("V"));
+    }
+
+    //test pour Bellmanford
+    public void testBellmanFordDistances(){
+        BellmanFord bf = new BellmanFord();
+        Valeurs res = new Valeurs();
+        assertEquals(0.0, res.getValeur("A"));
+        assertEquals(12.0, res.getValeur("B"));
+        assertEquals(76.0, res.getValeur("C"));
+    }
+
+    public void  testBellmanFordParents(){
+        BellmanFord bf = new BellmanFord();
+        Valeurs res = new Valeurs();
+        assertEquals("A", res.getParent("B"));
+        assertEquals("D", res.getParent("C"));
+    }
+
+    //test pour Djikstra
+    public void testDjikstraDistances(){
+        Djikstra dj = new Djikstra();
+        Valeurs res = new Valeurs();
+        assertEquals(0.0, res.getValeur("A"));
+        assertEquals(12.0, res.getValeur("B"));
+        assertEquals(76.0, res.getValeur("C"));
+    }
+
+    public void  testDjikstraParents(){
+        Djikstra dj = new Djikstra();
+        Valeurs res = new Valeurs();
+        assertEquals("A", res.getParent("B"));
+        assertEquals("D", res.getParent("C"));
+    }
+
 
 }
